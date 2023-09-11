e3388c6d7234e068345006a7d9723addd774cd67
shankar <shankarpenjarla1998@gmail.com>
2023-09-11 06:45:58 -0700

    installing git

---
+++ conditions.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+userid=$(id -u)
+
+  if[$userid -ne 0]
+  then
+    echo "please try to use root access"
+    exit 1
+  else
+    echo "Now you are good to install git "
+    fi
+
+ yum install git -y
+
+  
