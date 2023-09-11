298aaf92d557c69512c0aa2dfaa9616b7f995c32
shankar <shankarpenjarla1998@gmail.com>
2023-09-11 09:15:21 -0700

    mqsql packager

---
+++ install.mqsql.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+ 
+ userid=$(id -u)
+
+ validate(){
+    if [$1 -ne 0]
+    then 
+       echo "installation unsuccesfull"
+       exit 1
+    else 
+       echo "Installation success"
+       fi
+ }
+
+  if [$userid -ne 0]
+  then 
+    echo "Error:please access with root"
+    else
+    echo "you can install mqsql"
+    fi
+
+    yum install mysql -y
+
+    validate $? "mysql installation"
