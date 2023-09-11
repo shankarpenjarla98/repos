6ba1fe8a043e32902662bafb1da6de3cc06109f6
shankar <shankarpenjarla1998@gmail.com>
2023-09-11 00:31:27 -0700

    chatting

--- helloworld.sh
+++ helloworld.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-
+echo "Welcom to shell-scripting"
 
  PERSON1=shankar
  PERSON2=sai
 
  echo "$PERSON1: Hi,How are you $PERSON2"
- echo "$PERSON2:  yes,Iam fine $PERSON1"sssss
+ echo "$PERSON2:  yes,Iam fine $PERSON1"
+ echo "$PERSON1: what are doing  now"
+ echo "$PERSON2: Yeah, Iam working as devops engineer"
