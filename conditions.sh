c684920a3e69d3a815e3c0edd0053a5f1621e46d
shankar <shankarpenjarla1998@gmail.com>
2023-09-11 06:54:42 -0700

    modified again

--- conditions.sh
+++ conditions.sh
@@ -4,12 +4,12 @@
 userid=$(id -u)
 
   if[ $userid -ne 0 ]
-   then
+  then
     echo "please try to use root access"
     exit 1
   #else
    # echo "Now you are good to install git "
-    fi
+   fi
 
  yum install git -y
 
