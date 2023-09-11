43af1df2894ae7e25f956bcbdcbb4a0701aad014
shankar <shankarpenjarla1998@gmail.com>
2023-09-11 08:05:02 -0700

    modified2

--- conditions.sh
+++ conditions.sh
@@ -3,10 +3,11 @@
 
 userid=$(id -u)
 
-if[ $userid -ne 0 ]
+if [ $userid -ne 0 ]
+
 then
     echo "please run this script with root access"
-    exit 1
+    
   #else
    # echo "Now you are good to install git "
 fi
