9b4214987bf1335ff08b35ae2a92e035608199c2
shankar <shankarpenjarla1998@gmail.com>
2023-09-11 07:04:28 -0700

    sample condition

--- conditions.sh
+++ conditions.sh
@@ -3,14 +3,14 @@
 
 userid=$(id -u)
 
-  if[ $userid -ne 0 ]
-  then
-    echo "please try to use root access"
+if[ $userid -ne 0 ]
+then
+    echo "please run this script with root access"
     exit 1
   #else
    # echo "Now you are good to install git "
-   fi
+fi
 
- yum install git -y
+yum install git -y
 
   
---
+++ greaterthan.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+ num1=$1
+
+  if[ $num1 -gt 10 ]
+  then
+     echo "$num1 is greater than 10"
+  else
+     echo "$num1 is not greater than 10"
+   fi
