02bdf3bc44a0f239ef95f29ad21770c77c76fc1e
shankar <shankarpenjarla1998@gmail.com>
2023-09-11 01:07:23 -0700

    type3variable

---
+++ type3variable.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+person1=$1
+person2=$2
+
+echo "$PERSON1: Hi,How are you $PERSON2"
+echo "$PERSON2:  yes,Iam fine $PERSON1"
+echo "$PERSON1: what are doing  now"
+echo "$PERSON2: Yeah, Iam working as softwares engineer"
