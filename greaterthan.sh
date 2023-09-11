516f11a6f5de5e27714dec8c51affe211dc24d13
shankar <shankarpenjarla1998@gmail.com>
2023-09-11 07:24:34 -0700

    modified again

--- greaterthan.sh
+++ greaterthan.sh
@@ -2,9 +2,10 @@
 
  num1=$1
 
-  if[ $num1 -gt 10 ]
+  if[ $1 -gt 10 ]
   then
-     echo "$num1 is greater than 10"
+     echo "$1 is greater than 10"
+     exit 1
   else
-     echo "$num1 is not greater than 10"
+     echo "$1 is not greater than 10"
    fi
