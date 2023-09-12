4ac0873424046511cf07ef7288b17828602071de
shankar <shankarpenjarla1998@gmail.com>
2023-09-11 21:05:25 -0700

    install packages

---
+++ packagesinstall.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+userid1=$(id -u)
+date=$(date +%f)
+script_name=$0
+logfile=/tmp/$script_name-$date.log
+
+valid(){
+    if [$1 -ne 0]
+    then
+       echo $2 "installation package got failed"
+    else
+       echo  $2 "installation of package got success"
+    fi
+
+}
+
+if [$userid -ne 0]
+then
+   echo "error:please try login with sudo access that is root access to install any package"
+else
+   echo "your are good to go with installation"
+fi
+
+yum install git -y
+
+valid $? "git installation
+
+yum install mqsql -y
+
+valid $? "mysql installation"
+
+yum install postfix -y
+
+valid $? "postfix installation"
+
