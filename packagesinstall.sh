afea426a9f41d950df52dcb81ab37eff4f790529
shankar <shankarpenjarla1998@gmail.com>
2023-09-11 21:13:00 -0700

    pacakge-install

--- packagesinstall.sh
+++ packagesinstall.sh
@@ -8,14 +8,15 @@ logfile=/tmp/$script_name-$date.log
 valid(){
     if [$1 -ne 0]
     then
-       echo $2 "installation package got failed"
+       echo "$2  failed"
+       exit 1
     else
-       echo  $2 "installation of package got success"
+       echo  "$2  success"
     fi
 
 }
 
-if [$userid -ne 0]
+if [ $userid -ne 0 ]
 then
    echo "error:please try login with sudo access that is root access to install any package"
 else
