cb108f7a850072bb64e688d89401bdf0ce54c11b
shankar <shankarpenjarla1998@gmail.com>
2023-09-11 21:43:38 -0700

    git-installs

--- gitinstall.sh
+++ gitinstall.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 userac=$(id -u)
+date=$(date +%F)
+scriptfile=$0
+logfile1=/tmp/$scriptfile-$date.log
 
 validation(){
     if [ $1 -ne 0 ]
@@ -21,5 +24,11 @@ fi
 
 yum install git -y
 
-validation $? "git"
+validation $? "git" &>>$logfile1 
+
+yum install postfix -y 
+
+validation $? "postfix" &>>$logfile1
+
+
 
