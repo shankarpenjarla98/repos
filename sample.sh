e3fb0de67a58f7d8d0aedca380ef1efec48753be
shankar <shankarpenjarla1998@gmail.com>
2023-09-11 23:50:43 -0700

    samplefile

---
+++ sample.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+date=$(date +%F)
+logdir=/home/centos/scriptlogs
+accessid=$(id -u)
+logfile=$logdir-$date.log
+
+r="\e[31m"
+g="\e[32m"
+y="\e[33m"
+n="\e[0m"
+
+validate(){
+    if [$1 -ne 0 ]
+    then
+    echo -e " $r $2 installation got failed $n"
+    exit 1
+    else
+    echo -e "$g $2 installation got success $n"
+    fi
+
+}
+
+if [ $accessid -ne 0 ]
+then
+echo -e "$r error please try to install with root access $n "
+exit 1
+else
+echo -e "$y now you good to go for installation $n "
+fi
+
+for i in $@
+do
+yum list installed $i &>>$logfile
+if [ $? -ne 0 ]
+then
+echo "$i not installed let's install it"
+yum install git -y  &>>$logfile
+validate $? "$i"
+else
+echo "$y is already installed $n"
+fi
+done
